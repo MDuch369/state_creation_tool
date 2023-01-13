3e3e575c20e2d47635f14a4db2096ebf59cd8e52
Mateusz <mduch369@wp.pl>
2023-01-13 23:15:16 +0100

    constructor almost done (and so is my sanity)

--- state.cpp
+++ state.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <sstream>
 #include <fstream>
+#include <algorithm>
 #include "state.h"
 
 State::State(){}
@@ -12,24 +13,89 @@ State::State(const unsigned int &cur_line, const std::string &file) {
     std::ifstream  src(file, std::ios::binary);
     while(getline(src, line)) {
         copy_line++;
-        if(copy_line == cur_line - 3) { 
-            this->name = line.substr(0, line.find (" ") + 1);
+        if (copy_line >= cur_line - 3) {
+            if(compare_string("STATE", line)) { 
+                this->name = line.substr(0, line.find (" ") + 1);
+            }
+            if(compare_string("id", line)) { 
+                this->id = data_int(line);
+            }
+            if(compare_string("subsistence_building", line)) {
+             this->sub = data(line);
+            }
+            if(compare_string("provinces", line)) {
+                data_vector(this->provs, line, 7);
+            }
+            if(compare_string("impassable", line)) {
+                data_vector(this->im_provs, line, 7);
+            }
+
+            // TODO STATE TRAITS GO HERE
+
+            if(compare_string("city", line)) {
+                this->city = data(line);
+            }
+            if(compare_string("port", line)) {
+                this->port = data(line);
+            }
+            if(compare_string("farm", line)) {
+                this->farm = data(line);
+            }
+            if(compare_string("mine", line)) {
+                this->mine = data(line);
+            }
+            if(compare_string("wood", line)) {
+                this->wood = data(line);
+            }
+            if(compare_string("arable_land", line)) {
+                this->land = data_int(line);
+            }
+
+            // TODO ARABLE RESOURCES GO HERE
+
+            if(compare_string("bg_coal_mining", line)) {
+                this->coal = data_int(line);
+            }
+            if(compare_string("bg_iron_mining", line)) {
+                this->iron = data_int(line);
+            }
+            if(compare_string("bg_sulfur_mining", line)) {
+                this->sulfur = data_int(line);
+            }
+            if(compare_string("bg_logging", line)) {
+                this->log = data_int(line);
+            }
+            if(compare_string("bg_whaling", line)) {
+                this->whale = data_int(line);
+            }
+            if(compare_string("bg_fishing", line)) {
+                this->fish = data_int(line);
+            }
+            //      TODO FINISH DISCOVERABLE RESOURCES
+            if(compare_string("resource", line)) {
+                if(compare_string("bg_gold_fields", line)) {
+                    if(compare_string("undiscovered_amount", line)) {
+                        this->gold = data_int(line);
+                    }
+                    if(compare_string("discovered_amount", line)) {
+                        this->disc_gold = data_int(line);
+                    }                    
+                }
+                if(compare_string("bg_rubber", line)) {
+                    if(compare_string("undiscovered_amount", line)) {
+                        this->rubber = data_int(line);
+                    }
+                }
+                if(compare_string("bg_oil_extraction", line)) {
+                    if(compare_string("undiscovered_amount", line)) {
+                        this->oil = data_int(line);
+                    }                    
+                }                
+            }                                                                                                             
+            if(compare_string("naval_exit_id", line)) {
+                this->naval_exit = data_int(line);
+            }        
         }
-        if(copy_line == cur_line - 2) { 
-            this->id = data_int(line);
-         }
-        if(copy_line == cur_line - 1) {
-            this->sub = data(line);
-        }
-        if(copy_line == cur_line) {
-            data_vector(this->provs, line, 7);
-        }
-        // if(copy_line == cur_line + 1) {
-        //     data_vector(this->traits, line, 7);
-        // }
-        // if(copy_line == cur_line + 2) {
-        //     this->city = data(line);
-        // }
     }
     src.close();
 }
@@ -55,3 +121,14 @@ void data_vector(std::vector<std::string> &vec, const std::string &line, int len
         i++; 
     }
 }
+
+bool compare_string(const std::string &str, std::string l) {
+    l.erase (std::remove(l.begin(), l.end(), ' '), l.end());
+    return str == l.substr(0, str.size());
+}
+
+
+
+// void save_data ( const std::string &line, std::ifstream src) {
+//     getline(src, line);
+// }
--- state.h
+++ state.h
@@ -8,17 +8,18 @@
 
 class State {
 
-    std::vector<std::string> provs, traits, resources;
+    std::vector<std::string> provs, traits, resources, im_provs;
     std::string name, sub, city, port, farm, mine, wood;
-    unsigned int id, land, coal, iron, lead, sulfur, wood, fish, naval_exit;    
+    unsigned int id, land, coal, iron, lead, sulfur, log, fish, whale, oil, rubber, gold, disc_gold, naval_exit;    
 
     std::string data(const std::string);
     unsigned int data_int(const std::string);
     void data_vector(std::vector<std::string>&, const std::string&, int);
+    bool compare_string(const std::string&, std::string);
 
 public:
     State();
-    State(const unsigned int &cur_line, const std::string &file);
+    State(const unsigned int &, const std::string &);
 };
 
 
