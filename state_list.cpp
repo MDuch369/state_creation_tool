5c00c08c065061d083cefff299ea0ebe8d57246a
Mateusz <mduch369@wp.pl>
2023-05-19 12:47:45 +0200

    denest calculate_remaining_resources

--- state_list.cpp
+++ state_list.cpp
@@ -345,6 +345,188 @@ void State_list::save_state_builds(const std::filesystem::path &path/* , std::ve
         }
     }
 }
+
+void State_list::calculate_resources() {
+    double origin_provs{}, provs{};
+    double ratio{};
+    // Origin_state origin{states[this->origin_pos]};
+    for (std::shared_ptr<State> &state : this->states) {
+        std::shared_ptr<Transfer_state> transfer_state = std::dynamic_pointer_cast<Transfer_state>(state);
+        transfer_state->homelands = transfer_state->getOriginPtr()->getHomelands();
+        transfer_state->claims = transfer_state->getOriginPtr()->getClaims();
+        transfer_state->traits = transfer_state->getOriginPtr()->getTraits();
+        transfer_state->sub = transfer_state->getOriginPtr()->getSub();
+        transfer_state->ar_res = transfer_state->getOriginPtr()->getResources();
+        transfer_state->naval_exit = transfer_state->getOriginPtr()->getNavalExit();
+        
+        for(State::Country co : transfer_state->getOriginPtr()->getCountries()) {
+            origin_provs += co.getProvs().size();
+        }
+        for(State::Country co : transfer_state->getCountries()) {
+            provs += co.getProvs().size();
+        }
+        ratio = provs/origin_provs;
+        for(int i{}; i < 12; i++) {
+            transfer_state->res[i] = transfer_state->getOriginPtr()->getRes()[i] * ratio;
+        }
+    }
+}
+
+void prov_copy(State::Country &tar_co, State::Country &co) {
+    for(std::string prov : co.getProvs()) { // prov copying
+        bool present;
+        present = 0;
+        // WIP replacing loop with any_of algorithm
+        // if(std::any_of(tar_co.getProvs().begin(), tar_co.getProvs().end(), 
+            // [&prov](const std::string &target) { return target == prov; }))
+        // {
+        for(std::string tar_prov : tar_co.getProvs()) {
+            if(prov == tar_prov) {
+                present = 1;
+                break;
+            }
+        }
+        if(!present){
+            tar_co.getProvs().push_back(prov);
+        } 
+    }
+}
+void pop_copy(State::Country &tar_co, State::Country &co) 
+{
+    for(auto pop : co.getPops()) { // pop copying
+        bool present{};
+        for(auto tar_pop : tar_co.getPops()) {
+            if(pop.getCult() == tar_pop.getCult() && pop.getRel() == tar_pop.getRel()) {
+                tar_pop.setSize(tar_pop.getSize() + pop.getSize()); // ? refactor using operator overlading
+                present = 1;
+                break;
+            }
+        }
+        if(!present){tar_co.getPops().push_back(pop);}
+    }
+}
+void building_copy(State::Country &tar_co, State::Country &co)
+{
+  for(auto build : co.getBuilds()) { // building copying
+    bool present{};
+    for(auto tar_build : tar_co.getBuilds()) {
+        if(build.getType() == tar_build.getType()) {
+            tar_build.setLvl(tar_build.getLvl() + build.getLvl()); // ? refactor using operator overlading
+            present = 1;
+            break;
+        }
+    }
+    if(!present){tar_co.getBuilds().push_back(build);}
+    }  
+}
+//TODO denest
+void State_list::create_target_states(State_list &target_st/*, std::vector<State> &origin_st*/) { 
+    bool target{};
+    for(std::shared_ptr<State> &tr_st : this->states) {
+        for(std::shared_ptr<State> &st : target_st.getStates()) {
+            if(st->getName() == tr_st->getName()) {
+                for(State::Country co : tr_st->getCountries()) { // country copying
+                    for(State::Country& tar_co : st->getCountries()) {
+                        if(co.getName() == tar_co.getName()) {
+                            prov_copy(tar_co, co);
+                            pop_copy(tar_co, co); 
+                            building_copy(tar_co, co);
+                        }
+                    }
+                } 
+                int res[12]{};
+                for(int i{}; i < 11; i++) {
+                    res[i] = st->getRes()[i] + tr_st->getRes()[i];
+                }
+                st->setRes(res);
+                target = 1;
+                break;
+            }
+        }
+        if (!target) {
+            std::shared_ptr<Transfer_state> transfer_state = std::dynamic_pointer_cast<Transfer_state>(tr_st);
+            target_st.add_state(std::make_shared<Transfer_state>(*transfer_state));
+        } 
+    }
+}
+// ! this functions now handles a whole list of states, instead of one, so it should be carefully checked if it works properly
+void State_list::create_remaining_states(State_list &rem_st){ // ? merge with calculate_remaining_resources
+    bool found{};
+    for(std::shared_ptr<State> &state : this->states) {
+        std::shared_ptr<Transfer_state> transfer_state = std::dynamic_pointer_cast<Transfer_state>(state);
+        for(std::shared_ptr<State> &remaining_state : rem_st.getStates()) {
+            if (remaining_state->getName() == transfer_state->getOrigin()) {
+                found = 1;
+                break;
+            }
+        }
+        if(!found) {
+            // ! TODO test if this is corrrect
+            rem_st.getStates().emplace_back(std::move(transfer_state->getOriginPtr())); 
+        }
+    }
+}
+
+void subtract_pop(State::Country &co, State::Country &transfer_co) {
+    for(std::string& prov : co.getProvs()) { // prov subtracting
+        for(std::string transfer_prov : transfer_co.getProvs()) {
+            if(prov == transfer_prov) { // ? TODO refactor using operator overlading
+                prov = ""; // TODO erase empty provinces
+                break;
+            }
+        }
+    }
+}
+void subtract_pop(State::Country &co, State::Country &transfer_co) {
+    for(auto& pop : co.getPops()) { // pop subtracting
+        for(auto transfer_pop : transfer_co.getPops()) {
+            if(pop.getCult() == transfer_pop.getCult() && pop.getRel() == transfer_pop.getRel()) {
+                pop.setSize(pop.getSize() - transfer_pop.getSize()); // TODO refactor using operator overlading
+                break;
+            }    
+        }
+    }
+}
+void subtract_buildings(State::Country &co, State::Country &transfer_co) {
+    for(auto& build : co.getBuilds()) { // building subtracting
+        // bool present{};
+        for(auto transfer_build : transfer_co.getBuilds()) {
+            if(build.getType() == transfer_build.getType()) {
+                build.setLvl(build.getLvl() - transfer_build.getLvl()); // TODO refactor using operator overlading
+                break;
+            }
+        }
+    }
+}
+void subtract_country(std::shared_ptr<State> &remaining_state, std::shared_ptr<Transfer_state> &transfer_state){
+    for(State::Country& co : remaining_state->getCountries()) { // country subtracting
+        for(State::Country tr_co : transfer_state->getCountries()) {
+            if(co.getName() == tr_co.getName()) {
+                subtract_pop(co, tr_co);
+                subtract_buildings(co, tr_co);
+                // std::vector<std::string> provs{co.getProvs()};
+            }
+        }
+        co.getProvs().erase(std::remove(co.getProvs().begin(), co.getProvs().end(), ""), co.getProvs().end());
+    }
+}
+//  TODO denest
+void State_list::calculate_remaining_resources(State_list &rem_st/*, std::vector<State> &ori_st*/) {
+    for(std::shared_ptr<State> &state : this->states) {
+        for(std::shared_ptr<State> &remaining_st : rem_st.getStates()) {
+            // TODO check if origin_ptr can be used instead
+            std::shared_ptr<Transfer_state> transfer_state = std::dynamic_pointer_cast<Transfer_state>(state);
+            if(transfer_state->getOrigin() == remaining_st->getName()) {
+                subtract_country(remaining_st, transfer_state);
+                int res[/*st.getRes().size()*/12]{};
+                for(int i{}; i < 11; i++) {res[i] = remaining_st->getRes()[i] - transfer_state->getRes()[i];}
+                remaining_st->setRes(res);
+                break;
+            }
+        }
+    }
+}
+
 // ? consolidate the functions
 void State_list::add_state(std::shared_ptr<State> state) {
     // this->states.push_back(std::move(state));
